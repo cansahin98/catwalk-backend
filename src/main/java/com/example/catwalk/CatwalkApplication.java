dd5def49ac95ce8b84c3750ac727cec800163b1f
cansahin98 <70703577+cansahin98@users.noreply.github.com>
2022-01-30 00:31:39 +0300

    swagger ui

--- src/main/java/com/example/catwalk/CatwalkApplication.java
+++ src/main/java/com/example/catwalk/CatwalkApplication.java
@@ -2,8 +2,12 @@ package com.example.catwalk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@ComponentScan("com.example")
 public class CatwalkApplication {
 
 	public static void main(String[] args) {
